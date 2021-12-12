d0952b41d5a6a788d0ff802a4878e1a63f1cf663
Mateusz Grincewicz <matek160393@gmail.com>
2021-12-12 15:14:30 +0100

    adding new tasks and marking done ones

--- js/script.js
+++ js/script.js
@@ -2,28 +2,58 @@
     const tasks = [
         {
             content: "zapłacić rachunki",
-            done: false
+            done: true,
         },
         {
             content: "odkurzyć dywan",
-            done: true
+            done: true,
         },
     ];
 
     const render = () => {
         let htmlStrings = "";
+
         for (const task of tasks) {
+
             htmlStrings += `
-            <li>
+            <li
+             class="taskContent ${task.done ? "form__taskDone" : ""}">
             ${task.content}
             </li>
             `;
         }
+
         document.querySelector(".js-tasks").innerHTML = htmlStrings;
+    }
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        })
+
+        render();
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+
     }
 
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit)
+
     }
 
     init();
